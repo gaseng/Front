345e30d822b154ed969dd646344ecd412450a841
heo__ony <oueya1479@gmail.com>
2023-10-21 16:27:14 +0900

    Feat: localhost 범위 내 로그인 API  구현
    Reference: #7

---
+++ lib/models/login/login_response.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
+    LoginResponse(
+      memId: json['memId'] as int,
+      email: json['email'] as String,
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+    );
+
+Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
+    <String, dynamic>{
+      'memId': instance.memId,
+      'email': instance.email,
+      'accessToken': instance.accessToken,
+      'refreshToken': instance.refreshToken,
+    };
